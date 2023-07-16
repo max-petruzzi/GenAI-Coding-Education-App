90ff44b1bbef925fd979f4f6fe2087baeb972df3
Mdkar <mihirmdkar@gmail.com>
2023-07-15 20:33:42 -0400

    answer submission basic

--- flask-pwa-master/app/controller/main.py
+++ flask-pwa-master/app/controller/main.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, redirect
 )
 
 bp = Blueprint('main', __name__)
@@ -9,3 +9,20 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('main/index.html',
                            title='Flask-PWA')
+
+@bp.route('/student')
+def student():
+    return render_template('main/student.html',
+                           title='Student')
+
+@bp.route('/submit_form', methods=['POST'])
+def submit_form():
+    question = request.form.get('question-dropdown')
+    youtube_link = request.form.get('youtube-link')
+    print(question, youtube_link)
+
+    # Perform any necessary processing with the form data
+    # ...
+
+    # Redirect to another page after form submission
+    return redirect('/')
