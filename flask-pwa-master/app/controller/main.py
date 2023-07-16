801603ce59e0fc594e80ad7ecbb57aef0c502c7f
wz333 <yuriezhao@outlook.com>
2023-07-15 22:46:27 -0400

    Teacher screens

--- flask-pwa-master/app/controller/main.py
+++ flask-pwa-master/app/controller/main.py
@@ -10,6 +10,29 @@ def index():
     return render_template('main/index.html',
                            title='Flask-PWA')
 
+
+@bp.route('/teacher')
+def teacher():
+    return render_template('main/teacher.html',
+                           title='')
+
+@bp.route('/teacher/assignment')
+def teacher_assignment():
+    return render_template('main/teacher-creat-assignment.html',
+                           title='')
+@bp.route('/teacher/metrics')
+def teacher_metrics():
+    return render_template('main/teacher-metrics.html',
+                           title='')
+
+@bp.route('/teacher/submissions')
+def teacher_submissions():
+    return render_template('main/teacher-submissions.html',
+                           title='')
+
+
+
+
 @bp.route('/student')
 def student():
     return render_template('main/student.html',
