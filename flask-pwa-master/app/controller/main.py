b91e936ce9224f5065531ca81ed9f3d885e62e72
Mdkar <mihirmdkar@gmail.com>
2023-07-15 23:11:22 -0400

    student results

--- flask-pwa-master/app/controller/main.py
+++ flask-pwa-master/app/controller/main.py
@@ -1,9 +1,8 @@
 from flask import (
-    Blueprint, render_template, request, redirect
+    Blueprint, render_template, request, redirect, url_for
 )
 
 bp = Blueprint('main', __name__)
-
 
 @bp.route('/')
 def index():
@@ -15,14 +14,22 @@ def student():
     return render_template('main/student.html',
                            title='Student')
 
+@bp.route('/studentResults')
+def studentResults():
+    score = request.args.get('score')
+    question = request.args.get('question')
+    youtube_url = request.args.get('youtube_url')
+    return render_template('main/studentResults.html',
+                           title='Score', score=score, question=question, youtube_url=youtube_url)
+
 @bp.route('/submit_form', methods=['POST'])
 def submit_form():
     question = request.form.get('question-dropdown')
-    youtube_link = request.form.get('youtube-link')
-    print(question, youtube_link)
+    youtube_url = request.form.get('youtube-link')
+    score = 4.5
+    print(question, youtube_url, question)
 
-    # Perform any necessary processing with the form data
-    # ...
-
-    # Redirect to another page after form submission
-    return redirect('/')
+    return redirect(url_for('main.studentResults',
+                        question=question,
+                        youtube_url=youtube_url,
+                        score=score))
